97882804a3919d668f97fa347c961bf02a2fee5c
Guilherme Mingatto <guimingatto@gmail.com>
2026-08-13 09:59:37 -0300

    aula inteiros

---
+++ lib/tipos/inteiros.dart/inteiros.dart
@@ -0,0 +1,13 @@
+void main() {
+  int altura = 185;
+  print("altura: $altura cm");
+
+  int ano = 2026;
+  print("ano: $ano");
+
+  int idade = 31;
+  print("idade: $idade anos");
+
+  int datadenascimento = ano - idade;
+  print("data de nascimento: $datadenascimento");
+}
