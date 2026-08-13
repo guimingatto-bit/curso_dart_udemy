627fdb43c57c453470e37fc46ebf3dfb19c97c8e
Guilherme Mingatto <guimingatto@gmail.com>
2026-08-13 09:50:10 -0300

    variáveis

---
+++ lib/tipos/variáveis.dart
@@ -0,0 +1,25 @@
+void main() {
+  var nome = "Guilherme";
+  nome = "Guilherme Mingatto";
+
+  print(nome);
+
+  var idade = 31;
+  print(idade);
+
+  var peso = 93.5;
+
+  peso = 90;
+
+  print(peso);
+
+  var maiorIdade = true;
+
+  print(maiorIdade);
+
+  const nomeCompleto = "Guilherme Casanova Mingatto";
+  print(nomeCompleto);
+
+  final altura = 1.85;
+  print(altura);
+}
